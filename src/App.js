c0799081d9ed97f50f611626c08b181ec9112aca
David <destrada2193@gmail.com>
2021-09-21 15:34:54 -0700

    Complete application

--- src/App.js
+++ src/App.js
@@ -15,23 +15,35 @@ function App() {
     try {
       // const response = await fetch("https://swapi.dev/api/films/");
       const response = await fetch('https://react-udemy-http-fd441-default-rtdb.firebaseio.com/movies.json');
-      // note the ".../movies.json"
+      // note the ".../movies.json" is for Google firebase to create a new "node" where our data will be organized into
       
       if (response.status !== 200) {
         throw new Error('Oops. Something went wrong!');
       }
       
       const data = await response.json();
+
+      const loadedMovies = [];
+      // Initially, our loadedMovies array is empty, but we 'push' / add movies using the data we GET back 
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].releaseDate,
+        });
+      }
       
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      // setMovies(transformedMovies);
+// this was when we were using the Star Wars API^ 
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -42,8 +54,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-udemy-http-fd441-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    // we use fetch() to send a POST request by passing in a 2nd argument specifying the method key to POST and adding its associated 'body' which will be 'stringified' into JSON syntax, and lastly, our usual headers!
   }
   
   let content = <p>No movies found.</p>;
