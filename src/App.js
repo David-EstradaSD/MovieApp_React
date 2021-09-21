978a2123c0a9eddd8691e607381c7b7d5906616c
David <destrada2193@gmail.com>
2021-09-21 15:14:02 -0700

    Add AddMovie components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -8,18 +9,20 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
-
+      // const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch('https://react-udemy-http-fd441-default-rtdb.firebaseio.com/movies.json');
+      // note the ".../movies.json"
+      
       if (response.status !== 200) {
         throw new Error('Oops. Something went wrong!');
       }
       
       const data = await response.json();
-
+      
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -33,8 +36,16 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false); // we want to setIsLoading to false whether we have a failed HTTP request or retrieved a successsful request.
-  }
+  }, []);
 
+  useEffect( () => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+  
   let content = <p>No movies found.</p>;
 
   if (movies.length > 0) {
@@ -52,6 +63,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie = {addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
