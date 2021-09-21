883ea049610df090a6fc7bb37599d4b880d44e74
David <destrada2193@gmail.com>
2021-09-21 14:30:17 -0700

    Complete GET request for fetching movies and error handling logic for that request

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ const Movie = (props) => {
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      {/* since all of these prop names are different than the actual "model / api's names, we have to convert the names when we retrieve the data: SEE App.js!"  */}
     </li>
   );
 };
